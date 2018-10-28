d5bdee95bff5a55af2f52efc4d06f2440914f114
deividas406 <40849474+deividas406@users.noreply.github.com>
2018-10-28 11:12:26 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import {Component} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public showWebcam = false;
+  public errors: WebcamInitError[] = [];
+
+  public webcamImage: WebcamImage = null;
+
+  private trigger: Subject<void> = new Subject<void>();
+  private nextWebcam: Subject<boolean|string> = new Subject<boolean|string>();
+
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+
+  public toggleWebcam(): void {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info('received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  public get nextWebcamObservable(): Observable<boolean|string> {
+    return this.nextWebcam.asObservable();
+  }
+}
